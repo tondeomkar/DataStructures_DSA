02f0ba1f948eb248fd4750af6eddfda77635d0c4
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-09-29 19:30:08 +0530

    Add files via upload

---
+++ PBLMS_on_Array.cpp
@@ -0,0 +1,27 @@
+//swapping array
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10,20,30,40,50,60,70,80};
+    int size = 8;
+
+    int start = 0;
+    int end = size-1;
+
+    while(start<=end)
+    {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
